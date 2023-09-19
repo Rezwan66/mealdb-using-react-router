617bd1d7841b9a135304b5c2522c65bacea37571
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-20 00:54:53 +0600

    created react router on main.jsx file and added a home component to show header, outlet and footer

---
+++ src/components/Home/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar bg-base-100 flex justify-center">
+        <a className="btn btn-ghost normal-case text-xl">MealsDB</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line no-unused-vars
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
